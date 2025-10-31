8694fe0e33275bda78009b3f4b9f137ab1ec2612
Andrey <andrej5122785@yandex.ru>
2025-10-31 19:38:10 +0300

    test husky lint-staged

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => (
+  <div className="text-3xl font-bold text-blue-500">
+    Hello Cashflow Puzzle!
+  </div>
+);
+
+export default App;
+
